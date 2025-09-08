e0fc037ceed73993e49eee3d9ba6788d62f81c8b
BaldursGoonsack <149293305+BaldursGoonsack@users.noreply.github.com>
2025-09-09 02:38:48 +1000

    Lab version

---
+++ Ultimate NPC Stat Overhaul - Lab Edition/Mods/Ultimate_NPC_Stat_Overhaul/ScriptExtender/Lua/Server/_Subclasses.lua
@@ -0,0 +1,120 @@
+ClassData = {
+    Barbarian = {
+        MainPassive = "CX_Barbarian_Boost",
+        SubclassTable = {
+            "CX_Barbarian_Berserker_Boost",
+            "CX_Barbarian_WildMagic_Boost",
+            "CX_Barbarian_Wildheart_Boost"
+        },
+        SubclassUnlockLevel = 3
+    },
+    Bard = {
+        MainPassive = "CX_Bard_Boost",
+        SubclassTable = {
+            "CX_Bard_Lore_Boost",
+            "CX_Bard_Swords_Boost",
+            "CX_Bard_Valor_Boost"
+        },
+        SubclassUnlockLevel = 3
+    },
+    Cleric = {
+        MainPassive = "CX_Cleric_Boost",
+        SubclassTable = {
+            "CX_Cleric_Knowledge_Boost",
+            "CX_Cleric_Life_Boost",
+            "CX_Cleric_Light_Boost",
+            "CX_Cleric_Nature_Boost",
+            "CX_Cleric_Trickery_Boost",
+            "CX_Cleric_Tempest_Boost",
+            "CX_Cleric_War_Boost"
+        },
+        SubclassUnlockLevel = 1
+    },
+    Druid = {
+        MainPassive = "CX_Druid_Boost",
+        SubclassTable = {
+            "CX_Druid_Land_Boost",
+            "CX_Druid_Moon_Boost",
+            "CX_Druid_Spores_Boost"
+        },
+        SubclassUnlockLevel = 2
+    },
+    Fighter = {
+        MainPassive = "CX_Fighter_Boost",
+        SubclassTable = {
+            "CX_Fighter_BattleMaster_Boost",
+            "CX_Fighter_Champion_Boost",
+            "CX_Fighter_EldritchKnight_Boost"
+        },
+        SubclassUnlockLevel = 3
+    },
+    Monk = {
+        MainPassive = "CX_Monk_Boost",
+        SubclassTable = {
+            "CX_Monk_FourElements_Boost",
+            "CX_Monk_OpenHand_Boost",
+            "CX_Monk_Shadow_Boost"
+        },
+        SubclassUnlockLevel = 3
+    },
+    Paladin = {
+        MainPassive = "CX_Paladin_Boost",
+        SubclassTable = {
+            "CX_Paladin_Ancients_Boost",
+            "CX_Paladin_Devotion_Boost",
+            "CX_Paladin_Vengeance_Boost",
+            "CX_Paladin_Oathbreaker_Boost"
+        },
+        SubclassUnlockLevel = 1
+    },
+    Ranger = {
+        MainPassive = "CX_Ranger_Boost",
+        SubclassTable = {
+            "CX_Ranger_BeastMaster_Boost",
+            "CX_Ranger_GloomStalker_Boost",
+            "CX_Ranger_Hunter_Boost"
+        },
+        SubclassUnlockLevel = 3
+    },
+    Rogue = {
+        MainPassive = "CX_Rogue_Boost",
+        SubclassTable = {
+            "CX_Rogue_ArcaneTrickster_Boost",
+            "CX_Rogue_Assassin_Boost",
+            "CX_Rogue_Thief_Boost"
+        },
+        SubclassUnlockLevel = 3
+    },
+    Sorcerer = {
+        MainPassive = "CX_Sorcerer_Boost",
+        SubclassTable = {
+            "CX_Sorcerer_DraconicBloodline_Boost",
+            "CX_Sorcerer_WildMagic_Boost",
+            "CX_Sorcerer_StormSorcery_Boost"
+        },
+        SubclassUnlockLevel = 1
+    },
+    Warlock = {
+        MainPassive = "CX_Warlock_Boost",
+        SubclassTable = {
+            "CX_Warlock_Archfey_Boost",
+            "CX_Warlock_Fiend_Boost",
+            "CX_Warlock_GreatOldOne_Boost"
+        },
+        SubclassUnlockLevel = 1
+    },
+    Wizard = {
+        MainPassive = "CX_Wizard_Boost",
+        SubclassTable = {
+            "CX_Wizard_Abjuration_Boost",
+            "CX_Wizard_Conjuration_Boost",
+            "CX_Wizard_Divination_Boost",
+            "CX_Wizard_Enchantment_Boost",
+            "CX_Wizard_Evocation_Boost",
+            "CX_Wizard_Illusion_Boost",
+            "CX_Wizard_Necromancy_Boost",
+            "CX_Wizard_Transmutation_Boost"
+        },
+        SubclassUnlockLevel = 2
+    }
+}
